e0885481c2f847b22271025ea9ecd25a62c68071
damdaris <darisadam@upi.edu>
2024-09-14 22:42:31 +0700

    update

---
+++ TunnelOfCircles/TunnelOfCirclesApp.swift
@@ -0,0 +1,18 @@
+//
+//  TunnelOfCirclesApp.swift
+//  TunnelOfCircles
+//
+//  Created by Adam Daris Ryadhi on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TunnelOfCirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowResizability(.contentSize)
+    }
+}
